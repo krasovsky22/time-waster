cdbf9ff21216749695c8c96dbdb2150515b427b3
ukrasouski <ukrasouski@ambrygen.com>
2018-09-17 17:15:08 -0700

    arthurs treeviewer example 8)

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import { Jumbotron } from 'reactstrap'
 import CKEditorComponent from './CKEditor/CKEditorComponent'
+import TreeView from 'react-treeviewer'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+
+const data = [
+  {
+    id: 1,
+    text: 'Root',
+    //icon: <FontAwesomeIcon icon="stroopwafel" />,
+    expanded: false,
+    children: [
+      {
+        id: 3,
+        selected: true,
+        checked: false,
+        expanded: false,
+        text: 'Child 1',
+        icon: <FontAwesomeIcon icon="stroopwafel" size="2x" spin={true} style={{ color: 'red' }} />,
+        children: [{ id: 5, text: 'Grandchild 1', expanded: false }],
+      },
+    ],
+  },
+]
 
 class Body extends React.Component {
   render() {
     return (
       <main role="main" className="container">
         <Jumbotron>
-          <CKEditorComponent />
+          <TreeView data={data} />
         </Jumbotron>
       </main>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env = {}, args = {}) => {
       rules: [
         {
           test: /\.jsx$/,
-          exclude: /node_modules/,
+          //exclude: /node_modules/,
           use: [{ loader: 'babel-loader' }],
         },
         {
